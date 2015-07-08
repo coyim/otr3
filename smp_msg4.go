a02b38da365b78bb1dd705df1d5142a3fe346d07
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-07-08 18:29:12 -0500

    Implement message 4 generation and verification

---
+++ smp_msg4.go
@@ -0,0 +1,54 @@
+package otr3
+
+import (
+	"errors"
+	"math/big"
+)
+
+type smp4 struct {
+	y   *big.Int
+	r7  *big.Int
+	msg smpMessage4
+}
+
+type smpMessage4 struct {
+	cr *big.Int
+	d7 *big.Int
+	rb *big.Int
+}
+
+func (c *context) generateSMPFourthParameters(secret *big.Int, s2 smp2, m3 smpMessage3) smp4 {
+	s := c.generateFourthParameters()
+	s.y = secret
+	s.msg = calculateMessageFour(s, s2, m3)
+	return s
+}
+
+func (c *context) verifySMP4Parameters(msg2 smpMessage2, msg3 smpMessage3, msg smpMessage4) error {
+	if !c.isGroupElement(msg.rb) {
+		return errors.New("Rb is an invalid group element")
+	}
+
+	if !verifyZKP4(msg.cr, msg2.g3b, msg.d7, msg3.qaqb, msg.rb, 8) {
+		return errors.New("cR is not a valid zero knowledge proof")
+	}
+
+	return nil
+}
+
+func (c *context) generateFourthParameters() smp4 {
+	b := make([]byte, c.parameterLength())
+	s := smp4{}
+	s.r7 = c.randMPI(b)
+	return s
+}
+
+func calculateMessageFour(s smp4, s2 smp2, m3 smpMessage3) smpMessage4 {
+	var m smpMessage4
+
+	m.rb = modExp(m3.qaqb, s2.b3)
+	m.cr = hashMPIsBN(nil, 8, modExp(g1, s.r7), modExp(m3.qaqb, s.r7))
+	m.d7 = subMod(s.r7, mul(s2.b3, m.cr), q)
+
+	return m
+}
--- smp_test.go
+++ smp_test.go
@@ -457,3 +457,53 @@ func Test_verifySMP3Parameters_failsIfCrIsNotAValidZKP(t *testing.T) {
 	err := otr.verifySMP3Parameters(fixtureMessage1(), m)
 	assertDeepEquals(t, err, errors.New("cR is not a valid zero knowledge proof"))
 }
+
+func Test_generateSMPFourthParameters_generatesLongerValuesForR7WithProtocolV3(t *testing.T) {
+	otr := context{otrV3{}, fixtureRand()}
+	smp := otr.generateSMPFourthParameters(fixtureSecret(), fixtureSmp2(), fixtureMessage3())
+	assertDeepEquals(t, smp.r7, fixtureLong1)
+}
+
+func Test_generateSMPFourthParameters_generatesShorterValuesForR7WithProtocolV3(t *testing.T) {
+	otr := context{otrV2{}, fixtureRand()}
+	smp := otr.generateSMPFourthParameters(fixtureSecret(), fixtureSmp2(), fixtureMessage3())
+	assertDeepEquals(t, smp.r7, fixtureShort1)
+}
+
+func Test_generateSMPFourthParameters_computesRbCorrectly(t *testing.T) {
+	otr := context{otrV2{}, fixtureRand()}
+	smp := otr.generateSMPFourthParameters(fixtureSecret(), fixtureSmp2(), fixtureMessage3())
+	assertDeepEquals(t, smp.msg.rb, fixtureMessage4().rb)
+}
+
+func Test_generateSMPFourthParameters_computesCrCorrectly(t *testing.T) {
+	otr := context{otrV2{}, fixtureRand()}
+	smp := otr.generateSMPFourthParameters(fixtureSecret(), fixtureSmp2(), fixtureMessage3())
+	assertDeepEquals(t, smp.msg.cr, fixtureMessage4().cr)
+}
+
+func Test_generateSMPFourthParameters_computesD7Correctly(t *testing.T) {
+	otr := context{otrV2{}, fixtureRand()}
+	smp := otr.generateSMPFourthParameters(fixtureSecret(), fixtureSmp2(), fixtureMessage3())
+	assertDeepEquals(t, smp.msg.d7, fixtureMessage4().d7)
+}
+
+func Test_verifySMP4Parameters_succeedsForValidZKPS(t *testing.T) {
+	otr := context{otrV3{}, fixtureRand()}
+	err := otr.verifySMP4Parameters(fixtureMessage2(), fixtureMessage3(), fixtureMessage4())
+	assertDeepEquals(t, err, nil)
+}
+
+func Test_verifySMP4Parameters_failsIfRbIsNotInTheGroupForProtocolV3(t *testing.T) {
+	otr := context{otrV3{}, fixtureRand()}
+	err := otr.verifySMP4Parameters(fixtureMessage2(), fixtureMessage3(), smpMessage4{rb: big.NewInt(1)})
+	assertDeepEquals(t, err, errors.New("Rb is an invalid group element"))
+}
+
+func Test_verifySMP4Parameters_failsIfCrIsNotACorrectZKP(t *testing.T) {
+	otr := context{otrV3{}, fixtureRand()}
+	m := fixtureMessage4()
+	m.cr = sub(m.cr, big.NewInt(1))
+	err := otr.verifySMP4Parameters(fixtureMessage2(), fixtureMessage3(), m)
+	assertDeepEquals(t, err, errors.New("cR is not a valid zero knowledge proof"))
+}
