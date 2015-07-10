ba037e21cd4227e17d7448e72c4a65056cc49b3e
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-07-10 16:19:34 -0500

    Goes back to smpStateExpect1 when receiving an unexpected message

--- smp_state_machine.go
+++ smp_state_machine.go
@@ -10,7 +10,7 @@ type smpStateExpect4 struct{}
 var unexpectedMessageError = errors.New("unexpected SMP message")
 
 type smpMessage interface {
-	receiveMessage()
+	receivedMessage(smpState) (smpState, error)
 }
 
 type smpState interface {
@@ -20,82 +20,94 @@ type smpState interface {
 	receiveMessage4(smpMessage4) (smpState, error)
 }
 
-func (*smpStateExpect1) receiveMessage1(m smpMessage1) (smpState, error) {
-	return &smpStateExpect3{}, nil
+func (smpStateExpect1) receiveMessage1(m smpMessage1) (smpState, error) {
+	return smpStateExpect3{}, nil
 }
 
-func (*smpStateExpect1) receiveMessage2(m smpMessage2) (smpState, error) {
+func (smpStateExpect1) receiveMessage2(m smpMessage2) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect1) receiveMessage3(m smpMessage3) (smpState, error) {
+func (smpStateExpect1) receiveMessage3(m smpMessage3) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect1) receiveMessage4(m smpMessage4) (smpState, error) {
+func (smpStateExpect1) receiveMessage4(m smpMessage4) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect2) receiveMessage1(m smpMessage1) (smpState, error) {
+func (smpStateExpect2) receiveMessage1(m smpMessage1) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect2) receiveMessage2(m smpMessage2) (smpState, error) {
-	return &smpStateExpect4{}, nil
+func (smpStateExpect2) receiveMessage2(m smpMessage2) (smpState, error) {
+	return smpStateExpect4{}, nil
 }
 
-func (*smpStateExpect2) receiveMessage3(m smpMessage3) (smpState, error) {
+func (smpStateExpect2) receiveMessage3(m smpMessage3) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect2) receiveMessage4(m smpMessage4) (smpState, error) {
+func (smpStateExpect2) receiveMessage4(m smpMessage4) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect3) receiveMessage1(m smpMessage1) (smpState, error) {
+func (smpStateExpect3) receiveMessage1(m smpMessage1) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect3) receiveMessage2(m smpMessage2) (smpState, error) {
+func (smpStateExpect3) receiveMessage2(m smpMessage2) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect3) receiveMessage3(m smpMessage3) (smpState, error) {
-	return &smpStateExpect1{}, nil
+func (smpStateExpect3) receiveMessage3(m smpMessage3) (smpState, error) {
+	return smpStateExpect1{}, nil
 }
 
-func (*smpStateExpect3) receiveMessage4(m smpMessage4) (smpState, error) {
+func (smpStateExpect3) receiveMessage4(m smpMessage4) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect4) receiveMessage1(m smpMessage1) (smpState, error) {
+func (smpStateExpect4) receiveMessage1(m smpMessage1) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect4) receiveMessage2(m smpMessage2) (smpState, error) {
+func (smpStateExpect4) receiveMessage2(m smpMessage2) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect4) receiveMessage3(m smpMessage3) (smpState, error) {
+func (smpStateExpect4) receiveMessage3(m smpMessage3) (smpState, error) {
 	return nil, unexpectedMessageError
 }
 
-func (*smpStateExpect4) receiveMessage4(m smpMessage4) (smpState, error) {
-	return &smpStateExpect1{}, nil
+func (smpStateExpect4) receiveMessage4(m smpMessage4) (smpState, error) {
+	return smpStateExpect1{}, nil
 }
 
-func (m smpMessage1) receivedMessage(currentState smpState) {
-	currentState.receiveMessage1(m)
+func (m smpMessage1) receivedMessage(currentState smpState) (s smpState, err error) {
+	if s, err = currentState.receiveMessage1(m); err == unexpectedMessageError {
+		s = smpStateExpect1{}
+	}
+	return
 }
 
-func (m smpMessage2) receivedMessage(currentState smpState) {
-	currentState.receiveMessage2(m)
+func (m smpMessage2) receivedMessage(currentState smpState) (s smpState, err error) {
+	if s, err = currentState.receiveMessage2(m); err == unexpectedMessageError {
+		s = smpStateExpect1{}
+	}
+	return
 }
 
-func (m smpMessage3) receivedMessage(currentState smpState) {
-	currentState.receiveMessage3(m)
+func (m smpMessage3) receivedMessage(currentState smpState) (s smpState, err error) {
+	if s, err = currentState.receiveMessage3(m); err == unexpectedMessageError {
+		s = smpStateExpect1{}
+	}
+	return
 }
 
-func (m smpMessage4) receivedMessage(currentState smpState) {
-	currentState.receiveMessage4(m)
+func (m smpMessage4) receivedMessage(currentState smpState) (s smpState, err error) {
+	if s, err = currentState.receiveMessage4(m); err == unexpectedMessageError {
+		s = smpStateExpect1{}
+	}
+	return
 }
--- smp_state_machine_test.go
+++ smp_state_machine_test.go
@@ -6,7 +6,7 @@ func Test_smpStateExpect1_goToExpectState3WhenReceivesSmpMessage1(t *testing.T) 
 	state := smpStateExpect1{}
 	nextState, _ := state.receiveMessage1(smpMessage1{})
 
-	assertEquals(t, nextState, &smpStateExpect3{})
+	assertEquals(t, nextState, smpStateExpect3{})
 }
 
 func Test_smpStateExpect1_returnsAnErrorIfReceivesUnexpectedMessage(t *testing.T) {
@@ -25,7 +25,7 @@ func Test_smpStateExpect2_goToExpectState4WhenReceivesSmpMessage2(t *testing.T) 
 	state := smpStateExpect2{}
 	nextState, _ := state.receiveMessage2(smpMessage2{})
 
-	assertEquals(t, nextState, &smpStateExpect4{})
+	assertEquals(t, nextState, smpStateExpect4{})
 }
 
 func Test_smpStateExpect2_returnsAnErrorIfReceivesUnexpectedMessage(t *testing.T) {
@@ -44,7 +44,7 @@ func Test_smpStateExpect3_goToExpectState1WhenReceivesSmpMessage3(t *testing.T) 
 	state := smpStateExpect3{}
 	nextState, _ := state.receiveMessage3(smpMessage3{})
 
-	assertEquals(t, nextState, &smpStateExpect1{})
+	assertEquals(t, nextState, smpStateExpect1{})
 }
 
 func Test_smpStateExpect3_returnsAnErrorIfReceivesUnexpectedMessage(t *testing.T) {
@@ -63,7 +63,7 @@ func Test_smpStateExpect4_goToExpectState1WhenReceivesSmpMessage4(t *testing.T) 
 	state := smpStateExpect4{}
 	nextState, _ := state.receiveMessage4(smpMessage4{})
 
-	assertEquals(t, nextState, &smpStateExpect1{})
+	assertEquals(t, nextState, smpStateExpect1{})
 }
 
 func Test_smpStateExpect4_returnsAnErrorIfReceivesUnexpectedMessage(t *testing.T) {
