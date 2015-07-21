f1dd7813b50d8ef7c1786fcd0c21a58fde344937
Fan Jiang <fanjiang@thoughtworks.com>
2015-07-21 13:27:05 -0500

    add test for case when otrv2 is not allowed in policy

--- auth_state_machine.go
+++ auth_state_machine.go
@@ -23,22 +23,32 @@ func (c *akeContext) receiveMessage(msg []byte) (toSend []byte, err error) {
 		return nil, errors.New("otr: invalid OTR message")
 	}
 
-	if c.ignoreMessage(msg) {
-		return
-	}
-
 	switch msg[2] {
 	case msgTypeDHCommit:
+		if c.ignoreMessage(msg) {
+			return
+		}
 		c.authState, toSend = c.authState.receiveDHCommitMessage(c, msg)
 	case msgTypeDHKey:
+		if c.ignoreMessage(msg) {
+			return
+		}
 		c.authState, toSend = c.authState.receiveDHKeyMessage(c, msg)
 	case msgTypeRevealSig:
 		//TODO error
+		if !c.has(allowV2) {
+			//TODO error?
+			return
+		}
 		c.authState, toSend, _ = c.authState.receiveRevealSigMessage(c, msg)
 
 		//TODO set msgState = encrypted
 	case msgTypeSig:
 		//TODO error
+		if !c.has(allowV2) {
+			//TODO error?
+			return
+		}
 		c.authState, toSend, _ = c.authState.receiveSigMessage(c, msg)
 	default:
 		err = fmt.Errorf("otr: unknown message type 0x%X", msg[2])
