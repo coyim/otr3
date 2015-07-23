d1e26f69615491cd3073ead893638c7f1fc5ccbc
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-07-23 10:01:27 -0500

    Copy test from existing implementation

--- key_mgm_test.go
+++ key_mgm_test.go
@@ -42,3 +42,17 @@ func Test_calculateDHSessionKeys_failsWhenOurKeyIsUnknown(t *testing.T) {
 	_, err = c.calculateDHSessionKeys(1, 3)
 	assertDeepEquals(t, err, errors.New("otr: unexpected theirKeyID 3"))
 }
+
+func Test_calculateAKEKeys(t *testing.T) {
+	c := keyManagementContext{}
+
+	ssid, revealSigKeys, signatureKeys := c.calculateAKEKeys(expectedSharedSecret)
+
+	assertDeepEquals(t, ssid[:], bytesFromHex("9cee5d2c7edbc86d"))
+	assertDeepEquals(t, revealSigKeys.c[:], bytesFromHex("5745340b350364a02a0ac1467a318dcc"))
+	assertDeepEquals(t, signatureKeys.c[:], bytesFromHex("d942cc80b66503414c05e3752d9ba5c4"))
+	assertDeepEquals(t, revealSigKeys.m1[:], bytesFromHex("d3251498fb9d977d07392a96eafb8c048d6bc67064bd7da72aa38f20f87a2e3d"))
+	assertDeepEquals(t, revealSigKeys.m2[:], bytesFromHex("79c101a78a6c5819547a36b4813c84a8ac553d27a5d4b58be45dd0f3a67d3ca6"))
+	assertDeepEquals(t, signatureKeys.m1[:], bytesFromHex("b6254b8eab0ad98152949454d23c8c9b08e4e9cf423b27edc09b1975a76eb59c"))
+	assertDeepEquals(t, signatureKeys.m2[:], bytesFromHex("954be27015eeb0455250144d906e83e7d329c49581aea634c4189a3c981184f5"))
+}
