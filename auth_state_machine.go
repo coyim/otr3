5e3233d204ec1d9aa12c8a4d8a3e3ea9cae8683f
Ola Bini <ola@olabini.se>
2015-07-26 16:23:08 +0200

    Move query related functions out of auth_state_machine

--- auth_state_machine.go
+++ auth_state_machine.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
-	"strconv"
 )
 
 // ignoreMessage should never be called with a too small message buffer, it is assumed the caller will have checked this before calling it
@@ -51,28 +50,6 @@ func (c *conversation) receiveAKE(msg []byte) (toSend []byte, err error) {
 	return
 }
 
-func (c *conversation) receiveQueryMessage(msg []byte) (toSend []byte, err error) {
-	v, ok := acceptOTRRequest(c.policies, msg)
-	if !ok {
-		return nil, errInvalidVersion
-	}
-
-	//TODO set the version for every existing otrContext
-	c.version = v
-	c.senderInstanceTag = generateInstanceTag()
-
-	toSend, err = c.dhCommitMessage()
-
-	if err == nil {
-		c.ensureAKE()
-		c.ake.state = authStateAwaitingDHKey{}
-		c.keys.ourKeyID = 0
-		c.keys.ourCurrentDHKeys = dhKeyPair{}
-	}
-
-	return
-}
-
 type authStateBase struct{}
 type authStateNone struct{ authStateBase }
 type authStateAwaitingDHKey struct{ authStateBase }
@@ -93,44 +70,6 @@ type authState interface {
 
 func (authStateBase) receiveDHCommitMessage(c *conversation, msg []byte) (authState, []byte, error) {
 	return authStateNone{}.receiveDHCommitMessage(c, msg)
-}
-
-func parseOTRQueryMessage(msg []byte) []int {
-	ret := []int{}
-
-	if bytes.HasPrefix(msg, queryMarker) && len(msg) > len(queryMarker) {
-		versions := msg[len(queryMarker):]
-
-		if versions[0] == '?' {
-			ret = append(ret, 1)
-			versions = versions[1:]
-		}
-
-		if len(versions) > 0 && versions[0] == 'v' {
-			for _, c := range versions {
-				if v, err := strconv.Atoi(string(c)); err == nil {
-					ret = append(ret, v)
-				}
-			}
-		}
-	}
-
-	return ret
-}
-
-func acceptOTRRequest(p policies, msg []byte) (otrVersion, bool) {
-	versions := parseOTRQueryMessage(msg)
-
-	for _, v := range versions {
-		switch {
-		case v == 3 && p.has(allowV3):
-			return otrV3{}, true
-		case v == 2 && p.has(allowV2):
-			return otrV2{}, true
-		}
-	}
-
-	return nil, false
 }
 
 func (s authStateNone) receiveDHCommitMessage(c *conversation, msg []byte) (authState, []byte, error) {
---
+++ query.go
@@ -0,0 +1,66 @@
+package otr3
+
+import (
+	"bytes"
+	"strconv"
+)
+
+func parseOTRQueryMessage(msg []byte) []int {
+	ret := []int{}
+
+	if bytes.HasPrefix(msg, queryMarker) && len(msg) > len(queryMarker) {
+		versions := msg[len(queryMarker):]
+
+		if versions[0] == '?' {
+			ret = append(ret, 1)
+			versions = versions[1:]
+		}
+
+		if len(versions) > 0 && versions[0] == 'v' {
+			for _, c := range versions {
+				if v, err := strconv.Atoi(string(c)); err == nil {
+					ret = append(ret, v)
+				}
+			}
+		}
+	}
+
+	return ret
+}
+
+func acceptOTRRequest(p policies, msg []byte) (otrVersion, bool) {
+	versions := parseOTRQueryMessage(msg)
+
+	for _, v := range versions {
+		switch {
+		case v == 3 && p.has(allowV3):
+			return otrV3{}, true
+		case v == 2 && p.has(allowV2):
+			return otrV2{}, true
+		}
+	}
+
+	return nil, false
+}
+
+func (c *conversation) receiveQueryMessage(msg []byte) (toSend []byte, err error) {
+	v, ok := acceptOTRRequest(c.policies, msg)
+	if !ok {
+		return nil, errInvalidVersion
+	}
+
+	//TODO set the version for every existing otrContext
+	c.version = v
+	c.senderInstanceTag = generateInstanceTag()
+
+	toSend, err = c.dhCommitMessage()
+
+	if err == nil {
+		c.ensureAKE()
+		c.ake.state = authStateAwaitingDHKey{}
+		c.keys.ourKeyID = 0
+		c.keys.ourCurrentDHKeys = dhKeyPair{}
+	}
+
+	return
+}
