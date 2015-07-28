8b19361d040280bf0106a7a4a436dcb713bbff58
Fan Jiang <fanjiang@thoughtworks.com>
2015-07-28 16:36:42 -0500

    remove duplicate hexToBytes

--- helpers_test.go
+++ helpers_test.go
@@ -20,11 +20,6 @@ func assertDeepEquals(t *testing.T, actual, expected interface{}) {
 	}
 }
 
-func hexToByte(s string) []byte {
-	plainBytes, _ := hex.DecodeString(s)
-	return plainBytes
-}
-
 type fixedRandReader struct {
 	data []string
 	at   int
@@ -36,7 +31,7 @@ func fixedRand(data []string) io.Reader {
 
 func (frr *fixedRandReader) Read(p []byte) (n int, err error) {
 	if frr.at < len(frr.data) {
-		plainBytes := hexToByte(frr.data[frr.at])
+		plainBytes := bytesFromHex(frr.data[frr.at])
 		frr.at++
 		n = copy(p, plainBytes)
 		return
