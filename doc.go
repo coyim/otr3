6c8e5795fbb840f94819d613aa89f3b7c15e42f4
Fan Jiang <fanjiang@thoughtworks.com>
2015-08-17 10:26:55 -0500

    add doc.go for documentation

---
+++ doc.go
@@ -0,0 +1,31 @@
+// Package otr implements the Off The Record protocol as specified in:
+//  https://otr.cypherpunks.ca/Protocol-v3-4.0.0.html
+//
+// Introduction
+//
+// Off-the-Record (OTR) Messaging allows you to have private conversations over instant messaging by providing:
+//  Encryption
+//  Authentication
+//  Deniability
+//  Perfect forward secrecy
+//
+//
+// Getting Started
+//
+// OTR library provides a Conversation API for Receiving and Sending messages
+//  import otr3
+//
+//  c := &otr3.Conversation{}
+//
+//  // You will need to prepare a long-term PrivateKey for otr conversation handshakes.
+//  priv := &otr3.PrivateKey{}
+//  priv.Generate(rand.Reader)
+//  c.SetKeys(priv, nil)
+//
+//  // set the Policies.
+//  c.Policies.AllowV2()
+//  c.Policies.AllowV3()
+//
+//  // You can also setup a debug mode
+//  c.SetDebug(true)
+package otr3
