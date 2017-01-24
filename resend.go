feddfd721eda1d0d9891f0ce5aa33a4e7b8794bd
Ola Bini <ola@olabini.se>
2017-01-24 14:07:21 -0500

    Simplify copy of MessagePlaintext

--- resend.go
+++ resend.go
@@ -44,11 +44,7 @@ func (r *resendContext) later(msg MessagePlaintext, opaque ...interface{}) {
 	if r.messages.m == nil {
 		r.messages.m = make([]messageToResend, 0, 5)
 	}
-	mtr := messageToResend{
-		m:      append(MessagePlaintext(nil), msg...),
-		opaque: opaque,
-	}
-	r.messages.m = append(r.messages.m, mtr)
+	r.messages.m = append(r.messages.m, messageToResend{makeCopy(msg), opaque})
 }
 
 func (r *resendContext) pending() []messageToResend {
